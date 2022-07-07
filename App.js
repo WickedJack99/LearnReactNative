6d4722762a2621fd29eabf82c6a0cc7e04dd907e
WickedJack99 <aaronmoser@t-online.de>
2022-07-07 18:44:04 +0200

    0002

--- App.js
+++ App.js
@@ -1,43 +1,37 @@
-
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  //console.log("App executed"); //use console logs only for development because they slow down program execution.
 
-  const handlePress = () => console.log("Text clicked");
+  const [outputText, setOutputText] = useState('Starting Page');
 
-  
+  function setText(i) {
+    switch (i) {
+      case 0: {
+        setOutputText('Starting Page');
+      }break;
+      case 1: {
+        setOutputText('This app is my first approach to React Native');
+      }break;
+      case 2: {
+        setOutputText('WickedJack99');
+      }break;
+    }
+  }
 
   return (
-    //Like div
-    //ios: View -> UIView
-    //Self closing syntax to make code cleaner: instead of <Image></Image> use <Image />
-    //Use static images type: require("./assets/icon.png") in source
-    //https://i.picsum.photos/id/3/200/300.jpg?hmac=o1-38H2y96Nm7qbRf8Aua54lF97OFQSHR41ATNErqFc
-    <SafeAreaView style={styles.container}>
-      <Text onPress={handlePress}>Hello World!</Text>
-      
-      <Image source={{
-        width: 200,
-        height: 300,
-        uri: "https://i.picsum.photos/id/3/200/300.jpg?hmac=o1-38H2y96Nm7qbRf8Aua54lF97OFQSHR41ATNErqFc"}}/>
-      <StatusBar style="auto" />
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Text onPress={() => setText(0)}>{outputText}</Text>
+      <Text onPress={() => setText(1)}>About | <Text onPress={() => setText(2)}>Contact</Text></Text>
+    </View>
   );
 }
 
-//Creating an object "styles"
 const styles = StyleSheet.create({
-  //Container including all styles of object "styles".
   container: {
-    //Container is flexible, meaning it fills horizontically and vertically the free space
     flex: 1,
-    //Background-color of container is white(#fff)
     backgroundColor: '#fff',
-    //Content positioning at container
     alignItems: 'center',
     justifyContent: 'center',
-
   },
 });
